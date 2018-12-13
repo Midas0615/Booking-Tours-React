25e7f1b1eda06f7ec634741252b347baf97bdd62
midas0615 <marekjohn006@gmail.com>
2018-12-13 18:09:21 +0700

    tour route - merge em voi chi oi

--- src/routes.js
+++ src/routes.js
@@ -1,36 +1,24 @@
 import React from 'react';
-import HomePage from './pages/HomePage/HomePage';
 import NotFound from './pages/NotFound/NotFound';
-import ProductListPage from './pages/ProductListPage/ProductListPage';
-import ProductActionPage from './pages/ProductActionPage/ProductActionPage';
 import TourDetail from "./components/TourDetail/TourDetail";
 import Tours from './components/Tours/Tours';
+import Content from './components/Content/Content';
 
 const routes = [
     {
         path: '/',
         exact: true,
-        main: () => <HomePage />
-    },
-    {
-        path: '/product-list',
-        exact: false,
-        main: () => <ProductListPage />
-    },
-    {
-        path: '/product/add',
-        exact: false,
-        main: ({ location, history }) => <ProductActionPage location={location} history={history} />
-    },
-    {
-        path: '/product/:id/edit',
-        exact: false,
-        main: ({ match, history }) => <ProductActionPage match={match} history={history} />
+        main: ({ match}) => <Content match={match} />
     },
     {
         path: '/tours',
-        exact: false,
+        exact: true,
         main: ({ match}) => <Tours match={match} />
+    },
+    {
+        path: '/tours/:id',
+        exact: false,
+        main: ({ match}) => <TourDetail match={match} />
     },
     {
         path: '',
