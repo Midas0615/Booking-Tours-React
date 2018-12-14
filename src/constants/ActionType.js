05d40409cca479eb5a9ac3fd9d52651526911c48
midas0615 <marekjohn006@gmail.com>
2018-12-14 21:40:46 +0700

    user redux

--- src/constants/ActionType.js
+++ src/constants/ActionType.js
@@ -1,9 +1,3 @@
-export const FETCH_TOURS = 'FETCH_TOURS'; 
-export const ADD_TOUR = 'ADD_TOUR'; 
-export const UPDATE_TOUR = 'UPDATE_TOUR';
-export const DELETE_TOUR = 'DELETE_TOUR';
-export const EDIT_TOUR = 'EDIT_TOUR';
-
 export const GET_ALL_TOUR = 'GET_ALL_TOUR';
 export const GET_ALL_CATEGORY = 'GET_ALL_CATEGORY';
 export const CURRENT_CATEGORY = 'CURRENT_CATEGORY';
